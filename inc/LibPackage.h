139c105a52bcd96fcd879f5ccfc737a7fce3ee93
Akshay kumar <akshay_kumar@comcast.com>
2025-04-17 07:38:14 +0000

    Adding inc folder and moving header to inc

--- inc/LibPackage.h
+++ inc/LibPackage.h
@@ -2,7 +2,7 @@
  * If not stated otherwise in this file or this component's LICENSE file the
  * following copyright and licenses apply:
  *
- * Copyright 2021 Liberty Global Service B.V.
+ * Copyright 2020 RDK Management
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +15,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+*/
 
 #pragma once
 
 #include "IPackageImpl.h"
-#include <iostream>
 
 namespace packagemanager
 {
@@ -40,12 +39,10 @@ namespace packagemanager
 
         uint32_t GetLockInfo(const std::string &packageId, const std::string &version, std::string &unpackedPath, bool &locked) override;
 
-        //static std::shared_ptr<packagemanager::IPackageImpl> instance();
 
     private:
         LibPackage() = default;
-        static std::shared_ptr<packagemanager::IPackageImpl> instance;
     };
 
-} // namespace packagemanager
+}
 
