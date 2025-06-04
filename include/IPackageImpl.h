339c8f08048448e732e8e7a4ff0644712da77600
Josekutty Kuriakose <Josekutty_Kuriakose@cable.comcast.com>
2025-06-04 18:54:28 +0000

    Additional metadata fields

--- include/IPackageImpl.h
+++ include/IPackageImpl.h
@@ -15,6 +15,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ *
  */
 #pragma once
 
@@ -24,6 +26,7 @@
 #include <memory>
 #include <map>
 #include <vector>
+#include <set>
 
 namespace packagemanager
 {
@@ -33,6 +36,13 @@ namespace packagemanager
         SUCCESS,
         FAILED
     };
+
+    typedef enum : uint8_t
+    {
+        UNKNOWN = 0,
+        INTERACTIVE,
+        SYSTEM
+    } ApplicationType;
 
     struct ConfigMetaData
     {
@@ -45,25 +55,40 @@ namespace packagemanager
         uint32_t userId;
         uint32_t groupId;
         uint32_t dataImageSize;
+
+        bool resourceManagerClientEnabled;
+        std::string dialId;
+        std::string command;
+        ApplicationType appType;
+        std::string appPath;
+        std::string runtimePath;
+
+        std::string logFilePath;
+        uint32_t logFileMaxSize;
+        std::string logLevels; // json array of strings
+        bool mapi;
+        std::set<std::string> fkpsFiles;
+
+        std::string fireboltVersion;
+        bool enableDebugger;
     };
 
     typedef std::pair<std::string, std::string> ConfigMetadataKey;
     typedef std::map<ConfigMetadataKey, ConfigMetaData> ConfigMetadataArray;
 
     typedef std::vector<std::pair<std::string, std::string> > NameValues;
-
     class IPackageImpl
     {
     public:
         virtual ~IPackageImpl() = default;
 
-        virtual Result Initialize(const std::string &configStr, ConfigMetadataArray &configMetadata) = 0;
+        virtual Result Initialize(const std::string &configStr, ConfigMetadataArray &aConfigMetadata) = 0;
 
         virtual Result Install(const std::string &packageId, const std::string &version, const NameValues &additionalMetadata, const std::string &fileLocator, ConfigMetaData &configMetadata) = 0;
         virtual Result Uninstall(const std::string &packageId) = 0;
 
         virtual Result Lock(const std::string &packageId, const std::string &version, std::string &unpackedPath, ConfigMetaData &configMetadata) = 0;
-        virtual Result Unlock(const std::string &packageId, const std::string &version) = 0;
+        virtual Result Unlock(const std::string &packageId, const std::string &version) = 0; // XXX: Not in LibPackage-interface doc
 
         static std::shared_ptr<packagemanager::IPackageImpl> instance();
     };
